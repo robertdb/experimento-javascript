7aab13bb8e87c23e0766756eec3a3b6c99337c42
pabloernesto <caussabon@gmail.com>
2016-12-14 14:33:57 -0300

    moved myFunction and onAction to buttons.js

--- buttons.js
+++ buttons.js
@@ -1,8 +1,14 @@
 var image = "document.getElementById('pokemonImage')";
 var showBack = document.getElementById("showBack");
 var showFront = document.getElementById("showFront");
+var vacilar = document.getElementById("vacilar");
 
 showBack.setAttribute("onClick",
         image + ".src = 'charmander-back.png'");
 showFront.setAttribute("onClick",
         image + ".src = 'charmander-front.png'");
+vacilar.setAttribute("onClick", "myFunction()");
+
+function myFunction() {
+   document.getElementById("demo").innerHTML = "te la aplique gato.";
+}
